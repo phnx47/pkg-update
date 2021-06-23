9e413d213f7845a4798e1edb63772f4bc77f2b71
Serge K <cmt@phnx47.net>
2021-06-23 22:17:06 +0700

    feat: all.sh for cron

--- all.sh
+++ all.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-./firmware.sh
-./drop-cache.sh
-./mirror-list.sh
-./arch.sh
-./cargo.sh
-./yarn.sh
-./fisher.sh
-./vim-plug.sh
+SCRIPT_DIR=`dirname $0`
+echo $SCRIPT_DIR
+
+$SCRIPT_DIR/firmware.sh
+$SCRIPT_DIR/drop-cache.sh
+$SCRIPT_DIR/mirror-list.sh
+$SCRIPT_DIR/arch.sh
+$SCRIPT_DIR/cargo.sh
+$SCRIPT_DIR/yarn.sh
+fish $SCRIPT_DIR/fisher.sh
+$SCRIPT_DIR/vim-plug.sh
