ca47c40448c563a520338c245103318cbfb9fa4b
Serge K <cmt@phnx47.net>
2021-06-24 00:11:14 +0700

    chore: diable yarn

--- all.sh
+++ all.sh
@@ -3,11 +3,11 @@
 SCRIPT_DIR=`dirname $0`
 echo $SCRIPT_DIR
 
-$SCRIPT_DIR/firmware.sh
+#$SCRIPT_DIR/firmware.sh
 $SCRIPT_DIR/drop-cache.sh
 $SCRIPT_DIR/mirror-list.sh
 $SCRIPT_DIR/arch.sh
 $SCRIPT_DIR/cargo.sh
-$SCRIPT_DIR/yarn.sh
+#$SCRIPT_DIR/yarn.sh
 fish $SCRIPT_DIR/fisher.sh
 $SCRIPT_DIR/vim-plug.sh
---
+++ npm.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+npm update -g
+echo "Npm packages update finished!"
