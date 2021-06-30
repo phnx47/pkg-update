904196c056f987ee7f4a25c481ad44a2ae95acc8
Serge K <cmt@phnx47.net>
2021-06-30 21:30:48 +0700

    update mirror-list.sh

--- mirror-list.sh
+++ mirror-list.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export TMPFILE="$(mktemp)"
-rate-arch-mirrors --max-delay=10800 --entry-country SG  --protocol https --save=$TMPFILE && sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup && sudo mv $TMPFILE /etc/pacman.d/mirrorlist
+rate-mirrors --entry-country=SG --protocol=https --save=$TMPFILE arch --max-delay=10800 && sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup && sudo mv $TMPFILE /etc/pacman.d/mirrorlist
 
 echo "Mirrorlist update finished!!"
